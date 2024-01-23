e9f15f7ff27234c747d5ae9b5797013a701cc31c
Alexdosantos <alexdosantoss@yahoo.com>
2024-01-22 23:59:41 -0300

    Feature: ButtonProps

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,20 @@
+import * as S from "../Button/Button.Styled";
+
+const ButtonProps = () => {
+  return (
+    <>
+      <S.Button
+        customColors=""
+        customFontWeight=""
+        customFontsize=""
+        customHeight=""
+        customPadding=""
+        customWidth=""
+      >
+        Button
+      </S.Button>
+    </>
+  );
+};
+
+export default ButtonProps;
