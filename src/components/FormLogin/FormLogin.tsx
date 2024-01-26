971e8a0deabca6e232e32aaaae165f3ac50d090e
Alexdosantos <alexdosantoss@yahoo.com>
2024-01-26 17:25:51 -0300

    Feature: Validação do formulário de login

--- src/components/FormLogin/FormLogin.tsx
+++ src/components/FormLogin/FormLogin.tsx
@@ -1,7 +1,16 @@
 import * as S from "../FormLogin/FormLoginStyled";
+import { useForm } from "react-hook-form";
+import { loginSchema } from "../../schemas/schameogin";
+import { yupResolver } from "@hookform/resolvers/yup";
 import ButtonProps from "../Button/Button";
 
 const FormLogin = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ resolver: yupResolver(loginSchema) });
+
   const handleFake = () => {
     console.log("estou esperando funcionar");
   };
@@ -11,15 +20,25 @@ const FormLogin = () => {
         <S.TitleWelcome>Seja bem vindo!</S.TitleWelcome>
         <S.Titlelogin>Faça seu login</S.Titlelogin>
       </S.DivTitle>
-      <form action="">
+      <form onSubmit={handleSubmit(handleFake)}>
         <S.DivContainerInputs>
+          <p style={{ color: "red" }}>{errors.email?.message}</p>
           <S.DivInputEmail>
             <S.LabelEmail htmlFor="">E-mail</S.LabelEmail>
-            <S.InputEmail type="text" placeholder="Digite seu e-mail" />
+            <S.InputEmail
+              type="text"
+              placeholder="Digite seu e-mail"
+              {...register("email")}
+            />
           </S.DivInputEmail>
+          <p style={{ color: "red" }}>{errors.password?.message}</p>
           <S.DivInputPassword>
             <S.LabelPassword htmlFor="">Senha</S.LabelPassword>
-            <S.InputPassword type="password" placeholder="Digite sua senha" />
+            <S.InputPassword
+              type="password"
+              placeholder="Digite sua senha"
+              {...register("password")}
+            />
           </S.DivInputPassword>
         </S.DivContainerInputs>
         <S.DivButtonLogin>
