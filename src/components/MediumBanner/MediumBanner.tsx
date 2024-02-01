267d80c07761f5e598a445800485508dd3adaadf
Alexdosantos <alexdosantoss@yahoo.com>
2024-02-01 19:30:00 -0300

    Feature: Criação do componente Medium Banner

---
+++ src/components/MediumBanner/MediumBanner.tsx
@@ -0,0 +1,12 @@
+import * as S from '../MediumBanner/MediumBanner.Styled'
+import ImgMediumBanner from "../../assets/medium-banner.png";
+
+const MediumBanner = () => {
+  return (
+    <>
+      <S.ImgMediumBanner src={ImgMediumBanner} alt="" />
+    </>
+  );
+};
+
+export default MediumBanner;
--- src/pages/Home/Home.Styled.tsx
+++ src/pages/Home/Home.Styled.tsx
@@ -6,11 +6,20 @@ export const DivSearch = styled.div`
   justify-content: center;
   width: 100%;
   margin: -3% 0 0 0;
-  
+
   z-index: 2;
 
   @media screen and (max-width: 728px) {
-    
-    margin:-6% 0 0 0;
+    margin: -6% 0 0 0;
+  }
+`;
+
+export const DivMediumBanner = styled.div`
+  width: 100%;
+  align-items: center;
+  text-align: center;
+  margin: 8% 0 0 0;
+  @media screen and (max-width: 728px) {
+    margin: 15% 0 0 0;
   }
 `;
