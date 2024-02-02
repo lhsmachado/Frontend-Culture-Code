6f77d6441e157ae3a2c4ad667944a646303d9382
Alexdosantos <alexdosantoss@yahoo.com>
2024-02-02 00:13:46 -0300

    Feature: Foi realizado a implementação do Titulo na parte superior da seção produtos da home

--- src/pages/Home/Home.Styled.tsx
+++ src/pages/Home/Home.Styled.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 export const DivSearch = styled.div`
@@ -24,16 +25,84 @@ export const DivMediumBanner = styled.div`
   }
 `;
 
+export const DivTitle = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin: 0 auto;
+  width: 80%;
+  margin-top: 5%;
+
+  
+
+  @media screen and (max-width: 728px) {
+    display: none;
+  }
+`;
+
+export const Title = styled.h3`
+  font-family: Sora;
+  font-size: 1.5rem;
+  font-style: normal;
+  font-weight: 600;
+  line-height: 130%;
+  letter-spacing: 0.2px;
+  color: #212121;
+`;
+
+export const DivTitleResponsive = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin: 0 auto;
+
+  width: 100%;
+  margin-top: 5%;
+
+  
+
+  @media screen and (min-width: 728px) {
+    display: none;
+  }
+`;
+
+export const TitleResponsive = styled.h3`
+  padding: 0 0 0 6%;
+  font-family: Sora;
+  font-size: 1rem;
+  font-style: normal;
+  font-weight: 600;
+  line-height: 130%;
+  letter-spacing: 0.2px;
+  color: #212121;
+`;
+
+export const LinkviewProducts = styled(Link)`
+  padding: 0 6% 0 0;
+  font-family: Sora;
+  font-size: 1rem;
+  font-style: normal;
+  font-weight: 200;
+  line-height: 130%;
+  letter-spacing: 0.2px;
+  text-decoration: none;
+  color: #673cdf;
+
+  @media screen and (min-width: 768px) {
+    display: none;
+  }
+`;
+
 export const DivCardProducts = styled.div`
   display: flex;
+  flex-wrap: wrap;
   justify-content: center;
   gap: 30px;
   width: 100%;
   margin: 1% 0 0 0;
   align-items: center;
+  
 
   @media screen and (max-width: 728px) {
     flex-direction: column;
-    margin: 15% 0 0 0;
+    margin: 2% 0 0 0;
   }
 `;
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,11 @@
 import * as S from "../Home/Home.Styled";
-import ImgProducts from '../../assets/headset.png'
+import ImgProducts from "../../assets/headset.png";
 import CardBalance from "../../components/CardBalance/CardBalance";
 import FullBanner from "../../components/FullBanner/FullBanner";
 import Navigation from "../../components/Navigation/Navigation";
 import Search from "../../components/Search/Search";
 import MediumBanner from "../../components/MediumBanner/MediumBanner";
 import CardProduct from "../../components/CardProduct/CardProduct";
-
 
 const Home = () => {
   return (
@@ -19,15 +18,38 @@ const Home = () => {
         <Search />
       </S.DivSearch>
       <S.DivMediumBanner>
-        <MediumBanner/>
+        <MediumBanner />
       </S.DivMediumBanner>
-        
+
+      <S.DivTitle>
+        <S.Title>Produtos para você</S.Title>
+      </S.DivTitle>
+      <S.DivTitleResponsive>
+        <S.TitleResponsive>Para você</S.TitleResponsive>
+        <S.LinkviewProducts to={"/"}>{`Ver tudo >`} </S.LinkviewProducts>
+      </S.DivTitleResponsive>
+
       <S.DivCardProducts>
-        
-        <CardProduct image={ImgProducts} title="Headphone WB20BT Bluetooth" gems={50}   />
-        <CardProduct image={ImgProducts} title="Headphone WB20BT Bluetooth" gems={50}   />
-        <CardProduct image={ImgProducts} title="Headphone WB20BT Bluetooth" gems={50}   />
-        <CardProduct image={ImgProducts} title="Headphone WB20BT Bluetooth" gems={50}   />
+        <CardProduct
+          image={ImgProducts}
+          title="Headphone WB20BT Bluetooth"
+          gems={50}
+        />
+        <CardProduct
+          image={ImgProducts}
+          title="Headphone WB20BT Bluetooth"
+          gems={50}
+        />
+        <CardProduct
+          image={ImgProducts}
+          title="Headphone WB20BT Bluetooth"
+          gems={50}
+        />
+        <CardProduct
+          image={ImgProducts}
+          title="Headphone WB20BT Bluetooth"
+          gems={50}
+        />
       </S.DivCardProducts>
     </>
   );
