3efa80af6396f70963138cc39f97d576973676f4
Luís Machado <luis.kievel@gmail.com>
2024-02-07 19:10:25 -0300

    Feature: Product Redeem Card

--- src/components/ProfileCardProductRedeems/ProfileCardProductRedeems.tsx
+++ src/components/ProfileCardProductRedeems/ProfileCardProductRedeems.tsx
@@ -1,4 +1,5 @@
 import * as S from "./ProfileCardProductRedeemsStyle"
+import product from "../../assets/headset.png"
 
 const ProfileCardProductRedeems = () =>{
     return(
@@ -10,7 +11,26 @@ const ProfileCardProductRedeems = () =>{
                 <S.RedeemDate>22 de dezembro</S.RedeemDate>
             </div>
             <S.ProductList>
-                
+                <S.ProductCard>
+                    <div>
+                    <S.ProductImage src={product} />
+                    </div>
+                    <div>   
+                        <S.ProductName > Headphone W820BT Bluetooth over-ear edifier   </S.ProductName >
+                        <S.ProductPrice> 2 jóias </S.ProductPrice>
+                    </div>
+                    
+                </S.ProductCard>
+                <S.ProductCard>
+                    <div>
+                    <S.ProductImage src={product} />
+                    </div>
+                    <div>   
+                        <S.ProductName > Headphone W820BT Bluetooth over-ear edifier   </S.ProductName >
+                        <S.ProductPrice> 2 jóias </S.ProductPrice>
+                    </div>
+                    
+                </S.ProductCard>
             </S.ProductList>
 
         </S.Container>
--- src/components/ProfileCardProductRedeems/ProfileCardProductRedeemsStyle.tsx
+++ src/components/ProfileCardProductRedeems/ProfileCardProductRedeemsStyle.tsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const Container = styled.div `
     height: auto;
-    width: 31rem;
+    width: 42rem;
     border-radius: 3rem;
     background-color: #ffffff;
     box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
@@ -22,6 +22,8 @@ export const ProductList = styled.div `
     display: flex;
     flex-direction: row;
     flex-wrap: wrap;
+    gap: 1rem;
+    margin-top: 1rem;
 `
 
 export const RedeemDate = styled.h3`
@@ -31,6 +33,26 @@ export const RedeemDate = styled.h3`
 `
 
 export const ProductCard = styled.div `
-    
+    display: flex;
+    flex-direction: column;
+    width: 18.875rem;
+    max-height: 20rem;
+`
+export const ProductImage = styled.img`
+    width: 16rem;
+    height: 11rem;
+`
 
+export const ProductName = styled.h3`
+    font-family: Sora;
+    font-size: 14px;
+    font-weight: 600;
+    color: #212121;
+    margin-top: 1rem;
+`
+export const ProductPrice = styled.h5`
+    color: #9E9E9E;
+    font-family: Sora;
+    font-size: 14px;
+    font-weight: 400;
 `
