d609c2e538003df7d67bb91c6a20cef9c88fcb14
Luís Machado <luis.kievel@gmail.com>
2024-02-07 08:42:51 -0300

    Feature: Create Product Redeems Card

---
+++ src/components/ProfileCardProductRedeems/ProfileCardProductRedeems.tsx
@@ -0,0 +1,20 @@
+import * as S from "./ProfileCardProductRedeemsStyle"
+
+const ProfileCardProductRedeems = () =>{
+    return(
+        <S.Container>
+            <div>
+                <S.Title>Meus resgates</S.Title>
+            </div>
+            <div>
+                <S.RedeemDate>22 de dezembro</S.RedeemDate>
+            </div>
+            <S.ProductList>
+                
+            </S.ProductList>
+
+        </S.Container>
+    )
+}
+
+export default ProfileCardProductRedeems
---
+++ src/components/ProfileCardProductRedeems/ProfileCardProductRedeemsStyle.tsx
@@ -0,0 +1,36 @@
+import styled from "styled-components";
+
+export const Container = styled.div `
+    height: auto;
+    width: 31rem;
+    border-radius: 3rem;
+    background-color: #ffffff;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+    padding-left: 4rem;
+    padding-top: 3.75rem;
+    padding-bottom: 3.75rem;
+`
+
+export const Title = styled.h2`
+    font-weight: bold;
+    font-size: 1.5rem;
+    font-family: Sora;
+    color: #212121;
+
+`
+export const ProductList = styled.div `
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+`
+
+export const RedeemDate = styled.h3`
+    font-family: Sora;
+    font-size: 16px;
+    color: #757575;
+`
+
+export const ProductCard = styled.div `
+    
+
+`
