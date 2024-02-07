6d50a2fe2894441d74c179a76048dc34cfc4a5f2
Alexdosantos <alexdosantoss@yahoo.com>
2024-02-06 22:17:01 -0300

    Feature: implementação do cardSucessfullyRescued

--- src/pages/Products/Products.tsx
+++ src/pages/Products/Products.tsx
@@ -14,7 +14,7 @@ const Products = () => {
           myPerfil="Meu Perfil"
         />
       </S.DivNavigate>
-      {/* <CardProductsRedeems /> */}
+      <CardProductsRedeems />
       <CardSuccessfullyRescued/>
     </>
   );
