637cd07a7df7a27da24d8e0a96a0f472079e4531
Luís Machado <luis.kievel@gmail.com>
2024-02-13 19:17:11 -0300

    Fix: Corrigido erro no cardProduct

--- src/components/cardProduct/cardProduct.tsx
+++ src/components/cardProduct/cardProduct.tsx
@@ -1,8 +1,6 @@
 import * as S from "./CardProductStyled";
 import ButtonProps from "../Button/Button";
 
-import * as S from "./CardProductStyled";
-
 import Imgheart from "../../assets/heart-line.svg";
 
 interface ICardProductType {
--- src/pages/Perfil/Perfil.tsx
+++ src/pages/Perfil/Perfil.tsx
@@ -1,91 +1,89 @@
-import { useState } from "react"
-import Navigation from "../../components/Navigation/Navigation"
-import ProfileCardJewels from "../../components/ProfileCardJewels/ProfileCardJewels"
-import ProfileCardProductRedeems from "../../components/ProfileCardProductRedeems/ProfileCardProductRedeems"
-import ProfileCard from "../../components/ProfileCardUser/ProfileCardUser"
-import ProfileMenu from "../../components/ProfileMenu/ProfileMenu"
-import userImg from "../../assets/imguser.png"
-import returnArrow from "../../assets/arrowLeft.svg"
-import * as S from "./PerfilStyle"
+import { useState } from "react";
+import Navigation from "../../components/Navigation/Navigation";
+import ProfileCardJewels from "../../components/ProfileCardJewels/ProfileCardJewels";
+import ProfileCardProductRedeems from "../../components/ProfileCardProductRedeems/ProfileCardProductRedeems";
+import ProfileCard from "../../components/ProfileCardUser/ProfileCardUser";
+import ProfileMenu from "../../components/ProfileMenu/ProfileMenu";
+import userImg from "../../assets/imguser.png";
+import returnArrow from "../../assets/arrowLeft.svg";
+import * as S from "./PerfilStyle";
 
 const Perfil = () => {
-    const [selecteCard, setSelectCard] = useState('')
-    const screen = window.innerWidth
-    console.log(screen)
-    const handleChange = (value:string) =>{
-        setSelectCard(value)
+  const [selecteCard, setSelectCard] = useState("");
+  const screen = window.innerWidth;
+  console.log(screen);
+  const handleChange = (value: string) => {
+    setSelectCard(value);
+  };
+
+  const cardToRender = () => {
+    switch (selecteCard) {
+      case "profile":
+        return <ProfileCard />;
+      case "resgates":
+        return <ProfileCardProductRedeems />;
+      case "joias":
+        return <ProfileCardJewels />;
+      case "":
+        return <></>;
     }
+  };
 
-    const cardToRender = () => {
-        switch(selecteCard){
-            case 'profile':
-                return(<ProfileCard /> );
-            case 'resgates':
-                return(<ProfileCardProductRedeems />);
-            case 'joias':
-                return(<ProfileCardJewels />);
-            case '':
-                return(<></>)
-        }
+  const cardToRenderMobile = () => {
+    switch (selecteCard) {
+      case "profile":
+        return <ProfileCard />;
+      case "resgates":
+        return <ProfileCardProductRedeems />;
+      case "joias":
+        return <ProfileCardJewels />;
+      case "":
+        return <ProfileMenu onChange={handleChange} />;
     }
+  };
 
-    const cardToRenderMobile = () => {
-        switch(selecteCard){
-            case 'profile':
-                return(<ProfileCard /> );
-            case 'resgates':
-                return(<ProfileCardProductRedeems />);
-            case 'joias':
-                return(<ProfileCardJewels />);
-            case '':
-                return(<ProfileMenu onChange={handleChange}/>)
-        }
-    }
-
-    return(<>
-            {screen > 720 ? <S.ContainerPage>
-            
-            <div>
-            <Navigation 
-            customColorslinks="#000000"
-            home="Home"
-            produtos="Produtos"
-            myPerfil="Meu Perfil"/>
-            </div>
-            <S.ContainerBody>
-                <ProfileMenu onChange={handleChange}/>
-                {cardToRender()}
-
-            </S.ContainerBody>
-            
+  return (
+    <>
+      {screen > 720 ? (
+        <S.ContainerPage>
+          <div>
+            <Navigation
+              customColorslinks="#000000"
+              home="Home"
+              produtos="Produtos"
+              myPerfil="Meu Perfil"
+            />
+          </div>
+          <S.ContainerBody>
+            <ProfileMenu onChange={handleChange} />
+            {cardToRender()}
+          </S.ContainerBody>
         </S.ContainerPage>
-           : <>
-           <S.ContainerPage>
-            {
-                selecteCard == '' ?
-                <S.PictureContainer>
-                    <S.ProfilePicture src={userImg}/>
-                    <S.ProfileName>Alex</S.ProfileName>
-                </S.PictureContainer>
-                : <S.ReturnBar>
-                    <S.ReturnArrow src={returnArrow} onClick={() => setSelectCard('')}/>
-                    <S.HeaderText>Meu Perfil</S.HeaderText>
-                    <div></div>
-                </S.ReturnBar>
-            }
-            
-            <S.ContainerBody>
-                
-                {cardToRenderMobile()}
+      ) : (
+        <>
+          <S.ContainerPage>
+            {selecteCard == "" ? (
+              <S.PictureContainer>
+                <S.ProfilePicture src={userImg} />
+                <S.ProfileName>Alex</S.ProfileName>
+              </S.PictureContainer>
+            ) : (
+              <S.ReturnBar>
+                <S.ReturnArrow
+                  src={returnArrow}
+                  onClick={() => setSelectCard("")}
+                />
+                <S.HeaderText>Meu Perfil</S.HeaderText>
+                <div></div>
+              </S.ReturnBar>
+            )}
 
-            </S.ContainerBody>
-            
-        </S.ContainerPage>
-           </>}
-       
+            <S.ContainerBody>{cardToRenderMobile()}</S.ContainerBody>
+          </S.ContainerPage>
+        </>
+      )}
     </>
-        
-    )
-}
+  );
+};
 
-export default Perfil
+export default Perfil;
